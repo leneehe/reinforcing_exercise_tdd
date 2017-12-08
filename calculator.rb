0834766c90f617df51c8d8a0b844a96236407010
Lena <fatmeow@gmail.com>
2017-12-08 14:12:39 -0500

    Write all methods for the test output to run successfully

---
+++ calculator.rb
@@ -0,0 +1,18 @@
+class Calculator
+
+  def initialize
+  end
+
+  def add(number1, number2)
+    return number1 + number2
+  end
+
+  def subtract(number1, number2)
+    return number1 - number2
+  end
+
+  def sum(numbers)
+    return numbers.sum
+  end
+
+end
